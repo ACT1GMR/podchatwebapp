ccab71d0eafc94c4bc83ba6f60d27fe98e5edaa1
b.salarinia <act1gmr@gmail.com>
2018-10-16 15:50:55 +0330

    refactored

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -6,6 +6,7 @@ export const userGetToken = () => {
   return (dispatch) => {
     auth({
       clientId: "2051121e4348af52664cf7de0bda",
+      scope: "social:write",
       onNewToken: token => {
         dispatch({
           payload: token,
