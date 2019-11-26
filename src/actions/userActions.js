b248179c69d72835277a6f49670bd90ca7135a75
b.salarinia <act1gmr@gmail.com>
2019-11-26 15:46:21 +0330

    refactored

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -5,6 +5,7 @@ import {auth} from "podauth";
 export const userGetToken = () => {
   return (dispatch) => {
     auth({
+      ssoBaseUrl: "https://accounts.pod.ir/oauth2",
       clientId: "84994fec93b1402825b650db",
       scope: "social:write",
       onNewToken: token => {
